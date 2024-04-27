ad656f06603c01221668c56efb59e6b6a24835aa
yuzokanda <nlp2334@gmail.com>
2024-04-27 11:36:54 +0900

    rfm

--- app/Http/Controllers/Api/AnalysisController.php
+++ app/Http/Controllers/Api/AnalysisController.php
@@ -10,6 +10,7 @@ use App\Models\Order;
 use App\Models\Scopes\Subtotal;
 use App\Services\AnalysisService;
 use App\Services\DecileService;
+use App\Services\RFMService;
 
 class AnalysisController extends Controller
 {
@@ -33,12 +34,22 @@ class AnalysisController extends Controller
             list($data, $labels, $totals) = DecileService::decile($subQuery);
         }
 
+        if($request->type === 'rfm'){
+            list($data, $totals, $eachCount) = RFMService::rfm($subQuery, $request->rfmPrms);
+
+            return response()->json([
+                'data' => $data,
+                'type' => $request->type,
+                'eachCount' => $eachCount,
+                'totals' => $totals
+            ], Response::HTTP_OK);
+        }
+        
         return response()->json([
             'data' => $data,
             'type' => $request->type,
             'labels' => $labels,
-            'totals' => $totals
+            'totals' => $totals,
         ], Response::HTTP_OK);
     }
-
 }
--- resources/js/Components/ResultTable.vue
+++ resources/js/Components/ResultTable.vue
@@ -9,8 +9,8 @@ const priceNotation = (total) => Number(total).toLocaleString();
 </script>
 
 <template>
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-        <table v-if="data.type === 'perDay' || data.type === 'perMonth' || data.type === 'perYear'"  class="table-auto w-full text-left whitespace-no-wrap">
+    <div v-if="data.type === 'perDay' || data.type === 'perMonth' || data.type === 'perYear'" class="lg:w-2/3 w-full mx-auto overflow-auto">
+        <table class="table-auto w-full text-left whitespace-no-wrap">
             <thead>
             <tr>
                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">年月日</th>
@@ -25,8 +25,8 @@ const priceNotation = (total) => Number(total).toLocaleString();
             </tbody>
         </table>
     </div>
-    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
-        <table v-if="data.type === 'decile'" class="table-auto w-full text-left whitespace-no-wrap">
+    <div v-if="data.type === 'decile'" class="lg:w-2/3 w-full mx-auto overflow-auto">
+        <table class="table-auto w-full text-left whitespace-no-wrap">
             <thead>
             <tr>
                 <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">グループ</th>
@@ -42,6 +42,59 @@ const priceNotation = (total) => Number(total).toLocaleString();
                 <td class="border-b-2 border-gray-200 px-4 py-3">{{ priceNotation(item.totalPerGroup) }}</td>
                 <td class="border-b-2 border-gray-200 px-4 py-3">{{ item.totalRatio }}</td>
             </tr>
+            </tbody>
+        </table>
+    </div>
+    <div v-if="data.type === 'rfm'" class="lg:w-2/3 w-full mx-auto overflow-auto">
+
+        <h2 class="text-center text-3xl my-6">RFM 分析結果</h2>
+
+        <div class="my-4 text-center">合計: {{ data.totals }} 人</div>
+
+        <h3 class="text-center text-2xl my-4">RFMランク毎の人数</h3>
+
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">Rank</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">R</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">F</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">M</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="rfm in data.eachCount" :key="rfm.rank">
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rfm.rank }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rfm.r }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rfm.f }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rfm.m }}</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <h3 class="text-center text-2xl my-4">RとFの集計表</h3>
+
+        <table class="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+                <tr>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">rRank</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_5</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_4</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_3</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_2</th>
+                    <th class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">f_1</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="rf in data.data" :key="rf.rRank">
+                    <td>{{ rf.rRank }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rf.f_5 }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rf.f_4 }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rf.f_3 }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rf.f_2 }}</td>
+                    <td class="border-b-2 border-gray-200 px-4 py-3">{{ rf.f_1 }}</td>
+                </tr>
             </tbody>
         </table>
     </div>
